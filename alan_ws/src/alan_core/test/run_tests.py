19ac9b0495fc703e60bce75eff078bd7af39735f
Sergio <sorozco0612@yahoo.com>
2020-12-29 22:54:12 +0000

    commented out tests by accident

--- alan_ws/src/alan_core/test/run_tests.py
+++ alan_ws/src/alan_core/test/run_tests.py
@@ -5,7 +5,7 @@ import pi_video_stream
 import robot_movement
 
 # rosunit
-#rosunit.unitrun('alan_core', 'pi_video_stream',
-#                'pi_video_stream.PiCameraTestSuite')
+rosunit.unitrun('alan_core', 'pi_video_stream',
+                'pi_video_stream.PiCameraTestSuite')
 rostest.rosrun('alan_core', 'robot_movement',
                 'robot_movement.RobotMovementTestSuite')
