70cc9e7e4bf91fcd8fb2e8850684fc1480e5bada
Sergio <sorozco0612@yahoo.com>
2021-01-16 21:58:54 +0000

    Errors with calibration gui

--- alan_ws/src/alan_utilities/src/pid_calibration.py
+++ alan_ws/src/alan_utilities/src/pid_calibration.py
@@ -7,7 +7,7 @@ class PidCalibrationControl():
     def __init__(self):
         rospy.init_node('pid_calibration')
         self.pid_calibration_pub = rospy.Publisher('pid_calibration_pub',Float32MultiArray,queue_size=1)
-        self.create_and_display(widgets)
+        self.create_and_display_widgets()
 
     def create_and_display_widgets(self):
 
@@ -17,24 +17,24 @@ class PidCalibrationControl():
         self.kp_label = Label(self.root,text="Kp")
         self.kp_label.grid(row=0,column=0)
 
-        self.kp_entry = Entry(root)
+        self.kp_entry = Entry(self.root)
         self.kp_entry.grid(row=0,column=1)
 
         self.ki_label = Label(self.root,text="Ki")
         self.ki_label.grid(row=1,column=0)
 
-        self.ki_entry = Entry(root)
+        self.ki_entry = Entry(self.root)
         self.ki_entry.grid(row=1,column=1)
 
         self.kd_label = Label(self.root,text="Kd")
         self.kd_label.grid(row=2,column=0)
 
-        self.kd_entry = Entry(root)
-        self.kd_entry.grid(row=3,column=1)
+        self.kd_entry = Entry(self.root)
+        self.kd_entry.grid(row=2,column=1)
 
         self.accept_button = Label(self.root,text="Accept")
         self.accept_button.grid(row=4,column=1)
-        self.stream_button.bind('<Button-1>',self.accept_pid_settings)
+        self.accept_button.bind('<Button-1>',self.accept_pid_settings)
 
         self.root.mainloop()
 
