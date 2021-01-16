762a1e2c515c2bd5d7b5226dba85cac8cc9571c9
Sergio <sorozco0612@yahoo.com>
2021-01-16 21:54:51 +0000

    Adding a pid calibration GUI

---
+++ alan_ws/src/alan_utilities/src/pid_calibration.py
@@ -0,0 +1,47 @@
+#! /home/sorozco/computer_vision/bin/python3
+from tkinter import *
+from std_msgs.msg import Float32MultiArray
+import rospy
+
+class PidCalibrationControl():
+    def __init__(self):
+        rospy.init_node('pid_calibration')
+        self.pid_calibration_pub = rospy.Publisher('pid_calibration_pub',Float32MultiArray,queue_size=1)
+        self.create_and_display(widgets)
+
+    def create_and_display_widgets(self):
+
+        ## root widget (window)
+        self.root = Tk()
+
+        self.kp_label = Label(self.root,text="Kp")
+        self.kp_label.grid(row=0,column=0)
+
+        self.kp_entry = Entry(root)
+        self.kp_entry.grid(row=0,column=1)
+
+        self.ki_label = Label(self.root,text="Ki")
+        self.ki_label.grid(row=1,column=0)
+
+        self.ki_entry = Entry(root)
+        self.ki_entry.grid(row=1,column=1)
+
+        self.kd_label = Label(self.root,text="Kd")
+        self.kd_label.grid(row=2,column=0)
+
+        self.kd_entry = Entry(root)
+        self.kd_entry.grid(row=3,column=1)
+
+        self.accept_button = Label(self.root,text="Accept")
+        self.accept_button.grid(row=4,column=1)
+        self.stream_button.bind('<Button-1>',self.accept_pid_settings)
+
+        self.root.mainloop()
+
+    def accept_pid_settings(self):
+        kp = float(self.kp_entry.get())
+        ki = float(self.ki_entry.get())
+        kd = float(self.kd_entry.get())
+        self.pib_calibration_pub.publish(Float32MultiArray([kp,ki,kd]))
+
+control = PidCalibrationControl()
