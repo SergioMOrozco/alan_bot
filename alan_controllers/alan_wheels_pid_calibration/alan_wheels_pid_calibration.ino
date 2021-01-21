f002ab05731ee5c7803a0f2c53314e8be5561a4e
Sergio <sorozco0612@yahoo.com>
2021-01-21 04:27:19 +0000

    Finally got optimal PID values

--- alan_controllers/alan_wheels_pid_calibration/alan_wheels_pid_calibration.ino
+++ alan_controllers/alan_wheels_pid_calibration/alan_wheels_pid_calibration.ino
@@ -1,9 +1,10 @@
 #include <ros.h>
 #include "Arduino.h"
-#include <std_msgs/Float32MultiArray.h>
+#include <PID_v1.h>
 #include <std_msgs/Int32.h>
 #include <std_msgs/Float64.h>
 #include <stdlib.h>
+#include <PID_v1.h>
 
 // wheels
 #define LEFT_PWM 6
@@ -17,31 +18,30 @@
 
 //Prototypes
 void leftWheelControlEffortCallback(const std_msgs::Float64&);
+void rightWheelControlEffortCallback(const std_msgs::Float64&);
+
+unsigned long current_t = 0, prev_t = 0;
 
 //PID variables
-std_msgs::Float64 LeftWheelState, LeftWheelControlEffort, LeftWheelSetPoint;
-std_msgs::Float64 RightWheelState, RightWheelControlEffort, RightWheelSetPoint;
+//float Kp = 0.6, Ki = .01 , Kd = 10;
+//float Kp = 0.6, Ki = .008 , Kd = 100;
+float Kp = 0.3, Ki =0.01 , Kd = 1000;
+
+double LeftWheelState = 0, LeftWheelControlEffort = 0, LeftWheelSetPoint = 0;
+PID leftWheelPidController (&LeftWheelState, &LeftWheelControlEffort, &LeftWheelSetPoint, Kp, Ki, Kd, DIRECT); 
 
 ros::NodeHandle nh;
-ros::Publisher LeftWheelSetPointPublisher("setpoint",&LeftWheelSetPoint);
-ros::Publisher LeftWheelStatePublisher("state",&LeftWheelState);
-ros::Subscriber<std_msgs::Float64> LeftWheelControlEffortSubscriber("control_effort",&leftWheelControlEffortCallback);
 
 unsigned int left_tick = 0;
 unsigned int right_tick = 0;
 
 
 //motors
-int MaxRpm = 85;
+int MaxRpm = 180; // max rpm at 6v
 int PulsesPerRevolution = 384;
-
-
-void leftWheelControlEffortCallback(const std_msgs::Float64& control_effort) {
-	String lol = String("Control Effort" + String(control_effort.data));
-	char* lol_c = lol.c_str();
-	nh.loginfo(lol_c);
-	analogWrite(LEFT_PWM,control_effort.data);
-}
+int desiredPwm = 180;
+int minPwm = 115; //pwm where motor stops moving at 6v
+int maxPwm = 255;
 
 void setup() {
 	Serial.begin(115200);
@@ -51,11 +51,12 @@ void setup() {
 	TCCR1A = 0;
 	TCCR1B = 0;
 	OCR1A = 31249;
-	TCCR1B |= (1<<CS12) | (1<<WGM12); //prescaler 256, CTCMode
-	TIMSK1 |= (1<<OCIE1A); //enable timer overflow
+	OCR1B = 15624;
+	TCCR1B |= (1<<CS12) | (1<<WGM12);
+	TIMSK1 |= (1<<OCIE1A) | (1<<OCIE1B); //enable timer overflow
 
 	//specifically for pin 5 and 6: Reference - https://www.etechnophiles.com/change-frequency-pwm-pins-arduino-uno/
-	TCCR0B = TCCR0B & B11111000 | B00000010; // for PWM frequency of 7812.50 Hz
+	TCCR0B = TCCR0B & B11111000 | B00000001; // for PWM frequency of 62500.00 Hz
 
 	// hall effect sensor is an open drain output. This means that the
 	// sensor will only pull line low, but it won't pull it high. It will just
@@ -80,42 +81,73 @@ void setup() {
   	analogWrite(RIGHT_PWM, 0);
 
   	nh.initNode();
-	nh.advertise(LeftWheelSetPointPublisher);
-	nh.advertise(LeftWheelStatePublisher);
-	nh.subscribe(LeftWheelControlEffortSubscriber);
+
+	leftWheelPidController.SetOutputLimits(minPwm,maxPwm);
+
+	//turn PID Controllers on
+	leftWheelPidController.SetMode(AUTOMATIC);
 }
 
 ISR(TIMER1_COMPA_vect) // 0.5s timer
 {
-	double left_speed = 60.0 * (left_tick / (float)PulsesPerRevolution) / 0.5; //rpm
-	double right_speed = 60.0 * (right_tick / (float)PulsesPerRevolution) / 0.5; // rpm
 
-	if (left_speed > MaxRpm || right_speed > MaxRpm) 
-	{
-		nh.logwarn("Calculating RPM greater than max RPM. Adjust Max RPM.");
+	if (true) {
+		return;
 	}
 
+	double left_speed = 60 * (left_tick / (float)PulsesPerRevolution) / 0.5; //rpm
+	double right_speed = 60 * (right_tick / (float)PulsesPerRevolution) /0.5; // rpm
+
+
+	//right_tick = 0;
 	left_tick = 0;
-	right_tick = 0;
 
-	LeftWheelState.data = map(left_speed,0,MaxRpm,0,255);
-	RightWheelState.data = map(right_speed,0,MaxRpm,0,255);
+	LeftWheelState = map(left_speed,0,MaxRpm,minPwm,maxPwm);
 
-	String lol = String("State: " + String(LeftWheelState.data));
+	String lol = String("Left Tick: " + String(LeftWheelState));
 	char* lol_c = lol.c_str();
 	nh.loginfo(lol_c);
-	
-	LeftWheelStatePublisher.publish(&LeftWheelState);
+}
+
+ISR(TIMER1_COMPB_vect) // 0.25s timer
+{
+	if (false) {
+		return;
+	}
+
+	double left_speed = 60 * (left_tick / (float)PulsesPerRevolution) / 0.25; //rpm
+	double right_speed = 60 * (right_tick / (float)PulsesPerRevolution) /0.25; // rpm
+
+
+	//right_tick = 0;
+	left_tick = 0;
+
+	LeftWheelState = map(left_speed,0,MaxRpm,minPwm,maxPwm);
+
+	String lol = String("Left Tick: " + String(LeftWheelState));
+	char* lol_c = lol.c_str();
+	nh.loginfo(lol_c);
 }
 
 void loop() {
 	nh.spinOnce();
 
-	LeftWheelSetPoint.data = 128;
-	RightWheelSetPoint.data = 128;
+	LeftWheelSetPoint = desiredPwm;
 
-	LeftWheelSetPointPublisher.publish(&LeftWheelSetPoint);
-	delay(1000);
+	leftWheelPidController.Compute();
+
+
+
+	if (LeftWheelControlEffort == minPwm) {
+		digitalWrite(LEFT_FR,HIGH);
+		digitalWrite(LEFT_PWM,HIGH);
+	}
+	else {
+		digitalWrite(LEFT_FR,LOW);
+		analogWrite(LEFT_PWM, LeftWheelControlEffort);
+		//digitalWrite(LEFT_FR,HIGH);
+		//digitalWrite(LEFT_PWM,HIGH);
+	}
 }
 
 void left_encoder()
