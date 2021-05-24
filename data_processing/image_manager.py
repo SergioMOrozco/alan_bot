047d6ff2837e42f91ab6aee831ad87fce158ef40
Sergio Orozco <sorozco0612@yahoo.com>
2021-05-23 18:47:23 -0600

    using h5 file format to store large dataset

--- data_processing/image_manager.py
+++ data_processing/image_manager.py
@@ -1,32 +1,45 @@
 from cv2 import cv2
 import os
 import numpy as np
+from sklearn.model_selection import train_test_split
+import h5py
+
+filename = "data.h5"
 
 
 class ImageManager:
     def __init__(self):
         self.features = []
         self.labels = []
+        self.i = 0
+        self.batches = 500
 
     def start_menu(self):
 
-        directory = input(
+        self.directory = input(
             "Please input a directory containg .jpg files that you would like cleaned and made into a dataset: "
         )
-        self.create_dataset(directory)
-        self.save_dataset(directory)
-        self.mini_test(directory)
+
+        self.f = None
+
+        self.create_dataset(self.directory)
+
+        if not (self.features == []) and not (self.labels == []):
+            self.batches = len(self.features)
+            self.append_to_dataset()
+
+        # self.save_dataset(directory)
+        # self.mini_test(directory)
 
     def mini_test(self, directory):
         features = np.load(os.path.join(directory, "features.npy"))
         labels = np.load(os.path.join(directory, "labels.npy"))
 
-        cv2.imshow("image", features[500])
-        print(labels[500])
+        cv2.imshow("image", features[0])
+        print(labels[0])
         cv2.waitKey(1000)
 
     def create_dataset(self, search_directory):
-
         label_file = None
 
         # get label file
@@ -48,6 +61,11 @@ class ImageManager:
                 clean = ImageManager.clean_image(path)
 
                 if label_file:
+
+                    if self.i == self.batches:
+                        self.i = 0
+                        self.append_to_dataset()
+
                     # show image to user
                     # cv2.imshow("image", clean)
                     # cv2.waitKey(30)
@@ -61,12 +79,88 @@ class ImageManager:
                     label = label.split(",")
                     label = [float(item) for item in label]
 
+                    self.i += 1
+
                     # store label into dataset
                     self.labels.append([label[0], label[1]])
 
+    def append_to_dataset(self):
+        x_train, x_test, y_train, y_test = train_test_split(
+            np.array(self.features), np.array(self.labels)
+        )
+
+        print("Batch: ", self.batches)
+        if self.f is None:
+            self.f = h5py.File(os.path.join(self.directory, filename), "w")
+            train_x_data = self.f.create_dataset(
+                "x_train",
+                data=x_train,
+                compression="gzip",
+                chunks=True,
+                maxshape=(None, 432, 614, 3),
+            )
+            train_y_data = self.f.create_dataset(
+                "y_train",
+                data=y_train,
+                compression="gzip",
+                chunks=True,
+                maxshape=(None, 2),
+            )
+            test_x_data = self.f.create_dataset(
+                "x_test",
+                data=x_test,
+                compression="gzip",
+                chunks=True,
+                maxshape=(None, 432, 614, 3),
+            )
+            test_y_data = self.f.create_dataset(
+                "y_test",
+                data=y_test,
+                compression="gzip",
+                chunks=True,
+                maxshape=(None, 2),
+            )
+            print("'x_train' chunk has shape:{}".format(self.f["x_train"].shape))
+            print("'y_train' chunk has shape:{}".format(self.f["y_train"].shape))
+            print("'x_test' chunk has shape:{}".format(self.f["x_test"].shape))
+            print("'y_test' chunk has shape:{}".format(self.f["y_test"].shape))
+            print("====================")
+            self.f.close()
+
+        else:
+            hf = h5py.File(os.path.join(self.directory, filename), "a")
+
+            hf["x_train"].resize(hf["x_train"].shape[0] + x_train.shape[0], axis=0)
+            hf["x_train"][-x_train.shape[0] :] = x_train
+
+            hf["y_train"].resize(hf["y_train"].shape[0] + y_train.shape[0], axis=0)
+            hf["y_train"][-y_train.shape[0] :] = y_train
+
+            hf["x_test"].resize(hf["x_test"].shape[0] + x_test.shape[0], axis=0)
+            hf["x_test"][-x_test.shape[0] :] = x_test
+
+            hf["y_test"].resize(hf["y_test"].shape[0] + y_test.shape[0], axis=0)
+            hf["y_test"][-y_test.shape[0] :] = y_test
+
+            print("'x_train' chunk has shape:{}".format(hf["x_train"].shape))
+            print("'y_train' chunk has shape:{}".format(hf["y_train"].shape))
+            print("'x_test' chunk has shape:{}".format(hf["x_test"].shape))
+            print("'y_test' chunk has shape:{}".format(hf["y_test"].shape))
+            print("====================")
+
+            hf.close()
+
+        self.features = []
+        self.labels = []
+
     def save_dataset(self, directory):
-        np.save(os.path.join(directory, "features"), np.array(self.features))
-        np.save(os.path.join(directory, "labels"), np.array(self.labels))
+        x_train, x_test, y_train, y_test = train_test_split(
+            np.array(self.features), np.array(self.labels)
+        )
+        np.save(os.path.join(directory, "train_features"), x_train)
+        np.save(os.path.join(directory, "train_labels"), y_train)
+        np.save(os.path.join(directory, "test_features"), x_test)
+        np.save(os.path.join(directory, "test_labels"), y_test)
 
     @staticmethod
     def clean_image(image_path):
@@ -90,12 +184,12 @@ class ImageManager:
         ## canny = cv2.Canny(hsv_blur, 246, 255)
         # canny = cv2.Canny(hsv_blur, 0, 255)
 
-        region = ImageManager.region_of_interest(scaled)
+        # region = ImageManager.region_of_interest(scaled)
 
         # give single color channel. Needed for 2DConv
         # region = region.reshape(list(region.shape) + [1])
 
-        return region
+        return scaled
 
     @staticmethod
     def region_of_interest(img):
